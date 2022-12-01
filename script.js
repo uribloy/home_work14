a6cf192ce8bb9b0c61e720130f0d38dc1e4b6905
uribloy <uribloy@gmail.com>
2022-12-01 02:10:16 +0200

    style

--- script.js
+++ script.js
@@ -13,28 +13,28 @@ let posts = [
 let comments = [
     {'comntToPost' : '1',
     'commentTitle' : 'kishkush 1',
-    'bodycomment' : ' Lorem ipsum dolor sit amet consectetur adipisicing elit.  quisquam.'},
+    'bodycomment' : ' Lorem ipsum 11111111111111.'},
     {'comntToPost' : '1',
     'commentTitle' : 'balabush 1',
-    'bodycomment' : 'Maxime quia quas numquam mollitia, at, soluta dolorum rerum quod explicabo autem sit rem voluptate odio?.'},
+    'bodycomment' : 'Maxime quia 111111111111111.'},
     {'comntToPost' : '2',
     'commentTitle' : 'bla bla bla 2',
-    'bodycomment' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio harum sit id.'},
+    'bodycomment' : 'Lorem ipsum 22222222222222.'},
     {'comntToPost' : '2',
     'commentTitle' : 'aaaaaa 2',
-    'bodycomment' : 'Atque reprehenderit nisi quis consequatur autem, officia eum.'},
+    'bodycomment' : 'Atque reprehenderit 222222222222222.'},
     {'comntToPost' : '3',
     'commentTitle' : 'lorem 3',
-    'bodycomment' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque optio nostrum odio? Tenetur, qui enim?'},
+    'bodycomment' : 'Lorem ipsum 33333333333333.'},
     {'comntToPost' : '3',
     'commentTitle' : 'ipsum 3',
-    'bodycomment' : 'totam mollitia repellendus dicta recusandae molestiae doloremque. Officia, odit laudantium.'}
+    'bodycomment' : 'totam mollitia 3333333333333.'}
 ]
 function getpost (){
     return new Promise((resolve)=>{
         setTimeout(()=>{
             resolve(posts);
-        },2000);
+        },2500);
         mypage.push(posts);
     })
 }
@@ -42,22 +42,34 @@ function getcomment (){
     return new Promise((resolve)=>{
         setTimeout(()=>{
             resolve(comments);
-        },3500);
+        },4500);
         mypage.push(comments);
     })
 }
 
-function ptintToHTML(){
+// function ptintToHTML(){
+//     let content="";
+//     mypage[0].forEach((post) => {
+//         content += `<h2>${post.posttitle}</h2><p>${post.bodypost}</p>`;
+//         mypage[1].forEach((comment)=>{
+//             if (comment.comntToPost==post.postid){
+//             content+= `<h3>${comment.commentTitle}</h3><p>${comment.bodycomment}</p>`}
+//         })
+//     });
+//     document.body.innerHTML=content;
+// }
+
+getpost()
+.then(getcomment())
+.then(function ptintToHTML(){
     let content="";
     mypage[0].forEach((post) => {
-        content += `<h2>${post.posttitle}</h2><p>${post.bodypost}</p>`;
+        content += `<div class="post"><h2>${post.posttitle}</h2><p>${post.bodypost}</p></div>`
         mypage[1].forEach((comment)=>{
-            if (post.postid==comment.comntToPost){
-            content+= `<h3>${comment.commentTitle}</h3><p>${comment.bodycomment}</p>`}
+            if (comment.comntToPost==post.postid){
+            content+= `<div class=comment><h4>${comment.commentTitle}</h4><p>${comment.bodycomment}</p></div>`}
         })
     });
     document.body.innerHTML=content;
-}
-
-getpost().then(getcomment()).then(ptintToHTML());
-console.log(mypage);
+});
+console.log(mypage[1]);
